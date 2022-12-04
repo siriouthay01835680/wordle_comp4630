9b89dde1ba3fa55e17428e6f5b0742e478bd04e1
Josephine Luna <Josephine_Luna@student.uml.edu>
2022-12-03 20:24:19 -0500

    made a few aesthetic changes

--- app/src/main/java/com/mobileapp/wordle/GameFragment.java
+++ app/src/main/java/com/mobileapp/wordle/GameFragment.java
@@ -61,8 +61,8 @@ public class GameFragment extends Fragment {
                 //create new TextViews and set default attributes
                 gameGrid[i][j] = new TextView(view.getContext());
                 gameGrid[i][j].setTextColor(Color.BLACK);
-                gameGrid[i][j].setText("A");
-                gameGrid[i][j].setTextSize(14);
+                gameGrid[i][j].setText("W");
+                gameGrid[i][j].setTextSize(35);
                 gameGrid[i][j].setGravity(Gravity.CENTER);
                 gameGrid[i][j].setPadding(30,30,30,30);
 
@@ -70,7 +70,7 @@ public class GameFragment extends Fragment {
                 GridLayout.LayoutParams layoutParams=new GridLayout.LayoutParams();
                 layoutParams.setMargins(5, 5, 5, 5);
 
-                gameGrid[i][j].setHeight(200);
+                gameGrid[i][j].setHeight(180);
                 gameGrid[i][j].setWidth(195);
 
                 gameGrid[i][j].setBackgroundResource(R.color.off_white);
